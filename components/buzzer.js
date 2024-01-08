cbc94050502d20a0d430a8f61481e6ca08e138d6
S K <sebastienvasquez9@gmail.com>
2024-01-08 13:16:35 -0500

    deleted yoled sound
    test for real sound

--- components/buzzer.js
+++ components/buzzer.js
@@ -38,88 +38,6 @@ export default function Buzzer(props) {
     window.scrollTo(0, 0);
   });
 
-  (function () {
-    // Check if the browser supports web audio. Safari wants a prefix.
-    if ("AudioContext" in window || "webkitAudioContext" in window) {
-      //////////////////////////////////////////////////
-      // Here's the part for just playing an audio file.
-      //////////////////////////////////////////////////
-      var play = function play(audioBuffer) {
-        var source = context.createBufferSource();
-        source.buffer = audioBuffer;
-        source.connect(context.destination);
-        source.start();
-      };
-
-      var URL =
-        "https://s3-us-west-2.amazonaws.com/s.cdpn.io/123941/Yodel_Sound_Effect.mp3";
-      var AudioContext = window.AudioContext || window.webkitAudioContext;
-      var context = new AudioContext(); // Make it crossbrowser
-      var gainNode = context.createGain();
-      gainNode.gain.value = 1; // set volume to 100%
-      var yodelBuffer = void 0;
-
-      // The Promise-based syntax for BaseAudioContext.decodeAudioData() is not supported in Safari(Webkit).
-      window
-        .fetch(URL)
-        .then((response) => response.arrayBuffer())
-        .then((arrayBuffer) =>
-          context.decodeAudioData(
-            arrayBuffer,
-            (audioBuffer) => {
-              yodelBuffer = audioBuffer;
-            },
-            (error) => console.error(error)
-          )
-        );
-
-      // Ensure that playButton exists before assigning onclick
-      var playButton = document.getElementById("play");
-      if (playButton) {
-        playButton.onclick = function () {
-          return play(yodelBuffer);
-        };
-      }
-
-      //////////////////////////////////////////////////
-      // Here's the part for unlocking the audio context, probably for iOS only
-      //////////////////////////////////////////////////
-
-      // From https://paulbakaus.com/tutorials/html5/web-audio-on-ios/
-      // "The only way to unmute the Web Audio context is to call noteOn() right after a user interaction. This can be a click or any of the touch events (AFAIK – I only tested click and touchstart)."
-      /*
-      var unmute = document.getElementById("unmute");
-      unmute.addEventListener("click", unlock);
-
-      function unlock() {
-        console.log("unlocking");
-        // create empty buffer and play it
-        var buffer = context.createBuffer(1, 1, 22050);
-        var source = context.createBufferSource();
-        source.buffer = buffer;
-        source.connect(context.destination);
-
-        // play the file. noteOn is the older version of start()
-        source.start ? source.start(0) : source.noteOn(0);
-
-        // by checking the play state after some time, we know if we're really unlocked
-        setTimeout(function () {
-          if (
-            source.playbackState === source.PLAYING_STATE ||
-            source.playbackState === source.FINISHED_STATE
-          ) {
-            // Hide the unmute button if the context is unlocked.
-            unmute.style.display = "none";
-          }
-        }, 0);
-      }
-
-      // Try to unlock, so the unmute is hidden when not necessary (in most browsers).
-      unlock();
-      */
-    }
-  })();
-
   let game = props.game;
   let ws = props.ws;
 
@@ -230,6 +148,13 @@ export default function Buzzer(props) {
         html, body {
              overscroll-behavior: none;
         }
+
+        .center-container {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          height: 100vh;
+        }
       `}
         </style>
         <button
@@ -243,15 +168,15 @@ export default function Buzzer(props) {
         </button>
         {buzzerReg !== null ? (
           <>
-            <div className="flex flex-col items-center justify-center min-h-screen">
+            <div className="flex-col items-center justify-center min-h-screen">
               {!game.title && !game.is_final_round ? (
                 <div className="pt-8 flex flex-col space-y-5">
                   {/*<Round game={game} />*/}
 
                   {/* BUZZER BUTTON */}
                   <div
-                    className=""
-                    style={{ width: "110%", textAlign: "center" }}
+                    className="center-container"
+                    style={{ width: "100%", textAlign: "center" }}
                   >
                     {buzzed ? (
                       <>
